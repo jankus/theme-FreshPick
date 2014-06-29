3a4b1617f5023d041fc530d4f5f339fbe5e69f92
Audrius Jankauskas <audrius@impresspages.org>
2014-06-29 23:49:34 +0300

    Header, footer, configuration, widgets

--- _header.php
+++ _header.php
@@ -9,20 +9,11 @@
 
 <div id="content-wrap">
 
-    <!-- Header
-    ================================================== -->
     <header class="container">
         <?php echo ipSlot('logo'); ?>
-        <hgroup>
-            <h1><a href="index.htm">Freshpick</a></h1>
-            <h3>Your Awesome Slogan Here</h3>
-        </hgroup>
-
 
         <nav id="nav-wrap" class="cf">
-
             <?php echo ipSlot('menu', array('items' => 'menu1', 'attributes' => array('id' => 'menu'))); ?>
-
         </nav>
 
     </header>
